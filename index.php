7435777a8ea7be07f3e12bb1c4b8523dab7f885d
Isaac <isaac.bordas@gmail.com>
2018-02-03 11:46:15 +0100

    Format code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-function printLineBreak ()
+function printLineBreak()
 {
     return (PHP_SAPI === 'cli' ? PHP_EOL : '<br />');
 }
@@ -30,28 +30,34 @@ $searchbykeywords = new Filters\SearchKeywords(new \WordCounter\WordCounter($sen
 print 'Número total de coincidencias con las keywords (palabrejas, gañán, hiper-arquitecto, que, eh): ' . count($searchbykeywords->applyFilter()) . printLineBreak();
 
 $vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter($sentence));
-$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ', $vowel_filter->applyFilter())));
+$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ',
+    $vowel_filter->applyFilter())));
 print 'Número total de palabras que tienen más de dos caracteres y empiezan por vocal: ' . count($twochar_filter->applyFilter()) . printLineBreak();
 
 $keywords = ['palabrejas', 'gañán', 'hiper-arquitecto', 'que', 'eh'];
 $keywords_filter = new Filters\SearchKeywords(new \WordCounter\WordCounter($sentence), $keywords);
-$vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter(implode(' ', $keywords_filter->applyFilter())));
+$vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter(implode(' ',
+    $keywords_filter->applyFilter())));
 print 'Número total de keywords que empiezan por vocal: ' . count($vowel_filter->applyFilter()) . printLineBreak();
 
 $keywords = ['palabrejas', 'gañán', 'hiper-arquitecto', 'que', 'eh'];
 $keywords_filter = new Filters\SearchKeywords(new \WordCounter\WordCounter($sentence), $keywords);
-$vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter(implode(' ', $keywords_filter->applyFilter())));
-$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ', $vowel_filter->applyFilter())));
+$vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter(implode(' ',
+    $keywords_filter->applyFilter())));
+$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ',
+    $vowel_filter->applyFilter())));
 print 'Número total de keywords que empiezan por vocal y tienen más de dos caracteres: ' . count($twochar_filter->applyFilter()) . printLineBreak();
 
 $keywords = ['palabrejas', 'gañán', 'hiper-arquitecto', 'que', 'eh'];
 $keywords_filter = new Filters\SearchKeywords(new \WordCounter\WordCounter($sentence), $keywords);
-$notvowel_filter = new Filters\NotVowelStartingWords(new \WordCounter\WordCounter(implode(' ', $keywords_filter->applyFilter())));
+$notvowel_filter = new Filters\NotVowelStartingWords(new \WordCounter\WordCounter(implode(' ',
+    $keywords_filter->applyFilter())));
 print 'Número total de keywords que no empiezan por vocal: ' . count($notvowel_filter->applyFilter()) . printLineBreak();
 
 $notvowel_filter = new Filters\NotVowelStartingWords(new \WordCounter\WordCounter($sentence));
 $vowel_filter = new Filters\VowelStartingWords(new \WordCounter\WordCounter($sentence));
-$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ', $vowel_filter->applyFilter())));
+$twochar_filter = new Filters\MoreTwoCharsWords(new \WordCounter\WordCounter(implode(' ',
+    $vowel_filter->applyFilter())));
 $notvowelplusvowelandtwochar = count($twochar_filter->applyFilter()) + count($notvowel_filter->applyFilter());
 print 'Número total de palabras que no empiecen por vocal o que sí empiecen por vocal pero tengan mas de dos carácteres: ' . $notvowelplusvowelandtwochar . printLineBreak();
 
